7baf59ba89bb26c6f5774f4604431df99210eee7
Jen Calhoon <jcalhoon21@gmail.com>
2018-08-31 17:45:24 -0700

    setup navbar

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const morgan = require('morgan'); // 38.k (gzipped: 12.6K)
 const bodyParser = require('body-parser'); // 747.5k (gzipped: 256.1K)
 
 const app = express();
+var profile = require('./profile')
+
+// ...
+// then define the route that will use your custom router
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/profile', profile)
 
 // letting the app know where to find my template files
 app.set('views', './views');
@@ -15,9 +21,18 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // use res.render to send output of template by filename
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {    
     res.render('index');
 });
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+  });
+
+app.post('/thanks', (req, res) => {
+res.render('thanks', { contact: req.body })
+});
+
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
