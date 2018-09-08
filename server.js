eef4e5902aec2875d9ea1a6093583f43a76ad3d8
Jen Calhoon <jcalhoon21@gmail.com>
2018-09-07 21:20:41 -0700

    separated pages into home contact thanks.ejs

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express'); //565.6k (gzipped: 165.2k)
 const morgan = require('morgan'); // 38.k (gzipped: 12.6K)
 const bodyParser = require('body-parser'); // 747.5k (gzipped: 256.1K)
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 const app = express();
-var profile = require('./profile')
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
 
 // ...
 // then define the route that will use your custom router
@@ -15,7 +13,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.use('/profile', profile)
+
 
 // letting the app know where to find my template files
 app.set('views', './views');
@@ -33,21 +31,29 @@ app.get('/contact', (req, res) => {
   });
 
   app.post('/thanks', (req, res) => {
-    const { name, email, message } = req.body;
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    
+    // const { name, email, message } = req.body;
   
     const msg = {
-        to: 'test@example.com',
-        from: 'test@example.com',
+        to: 'jen_calhoon@yahoo.com',
+        from: req.body.email,
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
       };
-      sgMail.send(msg);
-  
-    res.render('thanks', { contact: req.body });
+      sgMail.send(msg, function(err){
+        if(err){
+            console.error('sendgrid: ' + err);
+        }
+        res.render('thanks', { contact: req.body });
+        console.log(process.env.SENDGRID_API_KEY);
+      });
   });
 
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
 console.log('listening at http://localhost:8080');
 });
+
+module.exports = app;
